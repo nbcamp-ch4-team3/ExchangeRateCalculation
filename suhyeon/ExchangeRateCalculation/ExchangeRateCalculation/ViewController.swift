bc18e3a2717d4f25d006cdbb8723daa06de7782b
LeeSuHyeon <tngus0673@naver.com>
2025-04-14 20:51:42 +0900

    chore: 프로젝트 세팅

---
+++ suhyeon/ExchangeRateCalculation/ExchangeRateCalculation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExchangeRateCalculation
+//
+//  Created by 이수현 on 4/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
