f1e5615c8ec4a13fa2b5f3b282b6ecfe57adda94
SongKyuSeob <sks5075@naver.com>
2025-04-15 14:37:28 +0900

    chore: 프로젝트 초기 no storyboard 관련 세팅

---
+++ ExchangeRateCalculation/ExchangeRateCalculation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExchangeRateCalculation
+//
+//  Created by 송규섭 on 4/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
