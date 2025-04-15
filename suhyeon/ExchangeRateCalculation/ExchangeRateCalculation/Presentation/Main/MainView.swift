1644a77eed8eacce5621ab5bd23ccf392df133d4
LeeSuHyeon <tngus0673@naver.com>
2025-04-15 13:17:44 +0900

    feat: Level 2. UISearchBar 구현

--- suhyeon/ExchangeRateCalculation/ExchangeRateCalculation/Presentation/Main/MainView.swift
+++ suhyeon/ExchangeRateCalculation/ExchangeRateCalculation/Presentation/Main/MainView.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class MainView: UIView {
+    let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "통화 검색"
+        searchBar.searchBarStyle = .minimal
+        return searchBar
+    }()
+
     let tableView: UITableView = {
         let tableView = UITableView()
         tableView.register(ExchangeRateCell.self, forCellReuseIdentifier: ExchangeRateCell.id)
@@ -37,12 +44,17 @@ private extension MainView {
     }
 
     func setHierarchy(){
-        self.addSubviews(views: tableView)
+        self.addSubviews(views: searchBar, tableView)
     }
 
     func setConstraints() {
+        searchBar.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide)
+            make.directionalHorizontalEdges.equalToSuperview()
+        }
         tableView.snp.makeConstraints { make in
-            make.directionalEdges.equalTo(safeAreaLayoutGuide)
+            make.top.equalTo(searchBar.snp.bottom)
+            make.directionalHorizontalEdges.bottom.equalTo(safeAreaLayoutGuide)
         }
     }
 }
