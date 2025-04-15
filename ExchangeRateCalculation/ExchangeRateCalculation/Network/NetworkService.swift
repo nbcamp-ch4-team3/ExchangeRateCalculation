17483702b51de7c6ce18df78f420d7384095b403
ChoiAnYong <dksdyd78@naver.com>
2025-04-15 23:43:37 +0900

    feat: NetworkService 에러 분기 추가

--- ExchangeRateCalculation/ExchangeRateCalculation/Network/NetworkService.swift
+++ ExchangeRateCalculation/ExchangeRateCalculation/Network/NetworkService.swift
@@ -10,26 +10,31 @@ import Foundation
 import Moya
 
 protocol NetworkServiceProtocol {
-    func getExchangeRate(nation: String) async throws -> ExchangeRateResponse
+    func getExchangeRate(nation: String) async throws -> ExchangeRateDTO
 }
 
 final class NetworkService: NetworkServiceProtocol {
     let provider = MoyaProvider<NetworkTargetType>()
     
-    func getExchangeRate(nation: String) async throws -> ExchangeRateResponse {
+    func getExchangeRate(nation: String) async throws -> ExchangeRateDTO {
         return try await withCheckedThrowingContinuation { continuation in
             provider.request(.getExchangeRate(nation: nation)) { result in
                 switch result {
                 case .success(let response):
+                    guard (200...299).contains(response.statusCode) else {
+                        continuation.resume(throwing: NetworkError.networkFail)
+                        return
+                    }
+                    
                     do {
-                        let responseData = try response.map(ExchangeRateResponse.self)
+                        let responseData = try response.map(ExchangeRateDTO.self)
                         
                         continuation.resume(returning: responseData)
                     } catch {
                         continuation.resume(throwing: NetworkError.decodeError)
                     }
-                case .failure(let error):
-                    continuation.resume(throwing: error)
+                case .failure:
+                    continuation.resume(throwing: NetworkError.etc)
                 }
             }
         }
