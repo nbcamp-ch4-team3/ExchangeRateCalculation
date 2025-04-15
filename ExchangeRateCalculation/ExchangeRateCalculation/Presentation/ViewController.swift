6383425ea36bffb61e20596c848bce6b07bd7905
ChoiAnYong <dksdyd78@naver.com>
2025-04-15 16:49:22 +0900

    chore: 프로젝트 세팅

---
+++ ExchangeRateCalculation/ExchangeRateCalculation/Presentation/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ExchangeRateCalculation
+//
+//  Created by 최안용 on 4/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    private let networkService = Service()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
