00803755999ad34215539ededafcca5982df4f6e
ChoiAnYong <dksdyd78@naver.com>
2025-04-15 23:43:01 +0900

    feat: ExchangeRateDTO 구현

---
+++ ExchangeRateCalculation/ExchangeRateCalculation/Domain/Entities/ExchangeRate.swift
@@ -0,0 +1,14 @@
+//
+//  ExchangeRate.swift
+//  ExchangeRateCalculation
+//
+//  Created by 최안용 on 4/15/25.
+//
+
+import Foundation
+
+struct ExchangeRate {
+    let currencyCode: String
+    let rate: String
+    let nation: String
+}
