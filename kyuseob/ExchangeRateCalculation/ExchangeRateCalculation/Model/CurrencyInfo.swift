3d2522b46e92e41df071db3a8ecf10370cf74254
SongKyuSeob <sks5075@naver.com>
2025-04-16 15:35:44 +0900

    feat: 셀에서 필요한 정보를 모델링한 CurrencyInfo 정의

--- kyuseob/ExchangeRateCalculation/ExchangeRateCalculation/Model/CurrencyInfo.swift
+++ kyuseob/ExchangeRateCalculation/ExchangeRateCalculation/Model/CurrencyInfo.swift
@@ -1,8 +1,11 @@
-//
-//  CurrencyInfo.swift
-//  ExchangeRateCalculation
-//
-//  Created by 송규섭 on 4/16/25.
-//
-
 import Foundation
+
+struct CurrencyInfo {
+    let code: String
+    let country: String
+    let rate: Double
+
+    var formattedRate: String {
+        return String(format: "%.4f", rate)
+    }
+}
