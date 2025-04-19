7a89b41c6b4e79710cbd618c5d38274f52f4c0f5
ChoiAnYong <dksdyd78@naver.com>
2025-04-19 16:54:47 +0900

    feat: CoredataStorage 구현

---
+++ anyong/ExchangeRateCalculation/ExchangeRateCalculation/Data/Repository/CoreDataStorage/CoreDataStorage.swift
@@ -0,0 +1,37 @@
+//
+//  CoreDataStorage.swift
+//  ExchangeRateCalculation
+//
+//  Created by 최안용 on 4/18/25.
+//
+
+import CoreData
+import UIKit
+
+final class CoreDataStorage {
+    static let shared = CoreDataStorage()
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "CoreDataStorage")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
