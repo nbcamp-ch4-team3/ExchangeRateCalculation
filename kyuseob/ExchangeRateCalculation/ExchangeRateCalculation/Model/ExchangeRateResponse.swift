ca955e19ed4271482930891fd770139718ea0659
SongKyuSeob <sks5075@naver.com>
2025-04-22 01:23:46 +0900

    feat: 새 기능에 맞춰 기존 CurrencyInfo, Response 구조 변경

--- kyuseob/ExchangeRateCalculation/ExchangeRateCalculation/Model/ExchangeRateResponse.swift
+++ kyuseob/ExchangeRateCalculation/ExchangeRateCalculation/Model/ExchangeRateResponse.swift
@@ -3,4 +3,11 @@ import Foundation
 struct ExchangeRateResponse: Codable {
     let result: String
     let rates: [String : Double]
+    let lastUpdatedDate: Date
+
+    enum CodingKeys: String, CodingKey {
+        case result = "result"
+        case rates = "rates"
+        case lastUpdatedDate = "time_last_update_utc"
+    }
 }
