6b33ec4df8b12018d3c8c491578eb8e0c5f06cc7
LeeSuHyeon <tngus0673@naver.com>
2025-04-22 17:55:14 +0900

    feat: LastScreen CoreData 구현

---
+++ suhyeon/ExchangeRateCalculation/LastScreen+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  LastScreen+CoreDataClass.swift
+//  ExchangeRateCalculation
+//
+//  Created by 이수현 on 4/22/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(LastScreen)
+public class LastScreen: NSManagedObject {
+
+}
---
+++ suhyeon/ExchangeRateCalculation/LastScreen+CoreDataProperties.swift
@@ -0,0 +1,24 @@
+//
+//  LastScreen+CoreDataProperties.swift
+//  ExchangeRateCalculation
+//
+//  Created by 이수현 on 4/22/25.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension LastScreen {
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<LastScreen> {
+        return NSFetchRequest<LastScreen>(entityName: "LastScreen")
+    }
+
+    @NSManaged public var screenName: String
+    @NSManaged public var currency: String?
+}
+
+extension LastScreen : Identifiable {
+
+}
