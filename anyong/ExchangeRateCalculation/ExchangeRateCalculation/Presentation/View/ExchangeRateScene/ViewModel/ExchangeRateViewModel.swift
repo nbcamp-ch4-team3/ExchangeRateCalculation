2f55c003444ef4fc72622ef8214e198c2f45a3a1
ChoiAnYong <dksdyd78@naver.com>
2025-04-23 16:15:19 +0900

    fix: 날짜 갱신 및 북마크 로직 수정

--- anyong/ExchangeRateCalculation/ExchangeRateCalculation/Presentation/View/ExchangeRateScene/ViewModel/ExchangeRateViewModel.swift
+++ anyong/ExchangeRateCalculation/ExchangeRateCalculation/Presentation/View/ExchangeRateScene/ViewModel/ExchangeRateViewModel.swift
@@ -10,17 +10,20 @@ import Foundation
 final class ExchangeRateViewModel: ViewModelProtocol {
     private let neworkService: NetworkService
     private let currencyCodeStorage: CurrencyCodeStorageService
+    private let updateDateStorage: UpdateDateStorageService
     
     enum Action {
         case fetch
         case search(text: String)
         case didTapStar(code: String, isSelected: Bool)
+        case setState(text: String)
     }
     
     struct State {
         var exchangeRates: [ExchangeRate] = []
         var searchRates: [ExchangeRate] = []
         var isSearching: Bool = false
+        var searchText: String = ""
         var errorMessage: String? = nil
     }
     
@@ -32,9 +35,14 @@ final class ExchangeRateViewModel: ViewModelProtocol {
     }
     var onStateChanged: ((State) -> Void)?
     
-    init(networkService: NetworkService, currencyCodeStorage: CurrencyCodeStorageService) {
+    init(
+        networkService: NetworkService,
+        currencyCodeStorage: CurrencyCodeStorageService,
+        updateDateStorage: UpdateDateStorageService
+    ) {
         self.neworkService = networkService
         self.currencyCodeStorage = currencyCodeStorage
+        self.updateDateStorage = updateDateStorage
         self.state = State()
         
         self.action = { [weak self] action in
@@ -47,6 +55,8 @@ final class ExchangeRateViewModel: ViewModelProtocol {
                 self.search(text: text)
             case let .didTapStar(code, isSelected):
                 self.didTapStarButton(code: code, isSelected: isSelected)
+            case let .setState(text):
+                self.state.searchText = text
             }
         }
     }
@@ -57,9 +67,13 @@ final class ExchangeRateViewModel: ViewModelProtocol {
             
             do {
                 let fetchedRates = try await self.neworkService.getExchangeRate(nation: "USD").toModel()
+//                let fetchedRates = self.neworkService.getMockData().toModel()
                 let savedCodes = try self.currencyCodeStorage.fetchAllData()
-                
+                 
                 if savedCodes.isEmpty {
+                    print("날짜 초기화")
+                    let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: Date())
+                    updateDateStorage.saveDate(yesterday!)
                     try fetchedRates.forEach { currency in
                         try self.currencyCodeStorage.saveData(
                             currency.currencyCode,
@@ -68,19 +82,29 @@ final class ExchangeRateViewModel: ViewModelProtocol {
                         )
                     }
                 }
+                
+                if let date = updateDateStorage.loadDate(),
+                   !date.isToday {
+                    print("날짜 갱신")
+                    updateDateStorage.updateDate(Date.now)
+                    
+                    try fetchedRates.forEach { rate in
+                        try self.currencyCodeStorage.updateExchangeRate(rate.currencyCode, rate.rate)
+                    }
+                }
 
-                let updatedRates = /*try */fetchedRates.map { rate in
+                let updatedRates = fetchedRates.map { rate in
                     var newRate = rate
                     if let index = savedCodes.firstIndex(where: { $0.code == rate.currencyCode }) {
                         newRate.isBookmark = savedCodes[index].isBookmark
-                        newRate.isFluctuation = self.checkIsFluctuation(rate.rate, savedCodes[index].exchangeRate)
-//                        try self.currencyCodeStorage.updateExchangeRate(rate.currencyCode, newRate.rate)
+                        newRate.fluctuationType = self.checkIsFluctuation(rate.rate, savedCodes[index].exchangeRate)
                     }
                     return newRate
                 }.sorted { $0.isBookmark && !$1.isBookmark }
                 
                 await MainActor.run {
                     self.state.exchangeRates = updatedRates
+                    self.search(text: self.state.searchText)
                 }
             } catch let error as NetworkError {
                 await MainActor.run {
@@ -100,11 +124,12 @@ final class ExchangeRateViewModel: ViewModelProtocol {
     
     private func search(text: String) {
         guard !text.isEmpty else {
+            state.searchText = ""
             state.searchRates = []
             state.isSearching = false
             return
         }
-        
+        state.searchText = text
         state.isSearching = true
         if text.isHangul {
             state.searchRates = state.exchangeRates.filter { $0.nation.contains(text) }
@@ -116,19 +141,28 @@ final class ExchangeRateViewModel: ViewModelProtocol {
     }
     
     private func didTapStarButton(code: String, isSelected: Bool) {
-        guard let index = state.exchangeRates.firstIndex(where: { $0.currencyCode == code}) else {
+        if let index = state.exchangeRates.firstIndex(where: { $0.currencyCode == code }) {
+            state.exchangeRates[index].isBookmark = isSelected
+            state.exchangeRates.sort { $0.currencyCode < $1.currencyCode }
+            state.exchangeRates.sort { $0.isBookmark && !$1.isBookmark }
+            
+            guard let index = state.searchRates.firstIndex(where: { $0.currencyCode == code}) else {
+                return
+            }
+            
+            state.searchRates[index].isBookmark = isSelected
+            state.searchRates.sort { $0.currencyCode < $1.currencyCode }
+            state.searchRates.sort { $0.isBookmark && !$1.isBookmark }
+
+        } else {
             return
         }
-        
-        state.exchangeRates[index].isBookmark = isSelected
-        state.exchangeRates.sort { $0.currencyCode < $1.currencyCode }
-        state.exchangeRates.sort { $0.isBookmark && !$1.isBookmark }
         
         do {
             if isSelected {
                 try currencyCodeStorage.updateIsBookmarked(code, isSelected)
             } else {
-                try currencyCodeStorage.removeData(code)
+                try currencyCodeStorage.updateIsBookmarked(code, isSelected)
             }
         } catch let error as CoreDataError {
             state.errorMessage = error.description
@@ -138,9 +172,9 @@ final class ExchangeRateViewModel: ViewModelProtocol {
     }
     
     private func checkIsFluctuation(_ newRate: Double, _ oldRate: Double) -> fluctuationType {
-        if newRate-oldRate > 0 {
+        if newRate - oldRate > 0 {
             return .up
-        } else if newRate-oldRate < 0 {
+        } else if newRate - oldRate < 0 {
             return .down
         } else {
             return .equal
